a48d06b5eda3e78cfa553faf762f9288b61c88e0
聽風踏雪 <liang_zhuang_dl@outlook.com>
2018-08-13 10:48:17 +0800

    添加用户详情类和地址类

---
+++ src/main/java/com/example/bookmanagementsystem/entity/authentication/Address.java
@@ -0,0 +1,104 @@
+package com.example.bookmanagementsystem.entity.authentication;
+
+import com.example.bookmanagementsystem.entity.BasicEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address extends BasicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String country;
+
+    @Column
+    private String province;
+
+    @Column
+    private String city;
+
+    @Column
+    private String district;
+
+    @Column
+    private String details;
+
+    @Column
+    private Boolean isMunicipality;
+
+    @Column
+    private Boolean isDelete;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Boolean getMunicipality() {
+        return isMunicipality;
+    }
+
+    public void setMunicipality(Boolean municipality) {
+        isMunicipality = municipality;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(Boolean delete) {
+        isDelete = delete;
+    }
+
+    public Address() {
+    }
+
+    public Address(String country, String province, String city, String district, String details, Boolean isMunicipality, Boolean isDelete) {
+        this.country = country;
+        this.province = province;
+        this.city = city;
+        this.district = district;
+        this.details = details;
+        this.isMunicipality = isMunicipality;
+        this.isDelete = isDelete;
+    }
+}
---
+++ src/main/java/com/example/bookmanagementsystem/entity/authentication/UserDetails.java
@@ -0,0 +1,143 @@
+package com.example.bookmanagementsystem.entity.authentication;
+
+import com.example.bookmanagementsystem.entity.BasicEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user_details")
+public class UserDetails extends BasicEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String realName;
+
+    @Column
+    private Boolean sex;
+
+    @Column
+    private String idCardNumber;
+
+    @Column
+    private String telephone;
+
+    @Column
+    private String cellphone;
+
+    @Column
+    private Date birthday;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_details_address", joinColumns = @JoinColumn(name = "user_details_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "address_id", referencedColumnName = "id"))
+    private Address address;
+
+    @Column
+    private String zipCode;
+
+    @Column
+    private Boolean isDelete;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_details_basic_user", joinColumns = @JoinColumn(name = "user_details_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "basic_user_id", referencedColumnName = "id"))
+    private BasicUser basicUser;
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public Boolean getSex() {
+        return sex;
+    }
+
+    public void setSex(Boolean sex) {
+        this.sex = sex;
+    }
+
+    public String getIdCardNumber() {
+        return idCardNumber;
+    }
+
+    public void setIdCardNumber(String idCardNumber) {
+        this.idCardNumber = idCardNumber;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(Boolean delete) {
+        isDelete = delete;
+    }
+
+    public BasicUser getBasicUser() {
+        return basicUser;
+    }
+
+    public void setBasicUser(BasicUser basicUser) {
+        this.basicUser = basicUser;
+    }
+
+    public UserDetails() {
+    }
+
+    public UserDetails(String realName, Boolean sex, String idCardNumber, String telephone, String cellphone, Date birthday, Address address, String zipCode, Boolean isDelete, BasicUser basicUser) {
+        this.realName = realName;
+        this.sex = sex;
+        this.idCardNumber = idCardNumber;
+        this.telephone = telephone;
+        this.cellphone = cellphone;
+        this.birthday = birthday;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.isDelete = isDelete;
+        this.basicUser = basicUser;
+    }
+}
